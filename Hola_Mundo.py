a7ac330163bad865efa3032af61bfc569c9a5ef1
UlisesFalconISC <143455317+UlisesFalconISC@users.noreply.github.com>
2023-10-03 17:58:31 -0600

    Se agregan los archivos

---
+++ Hola_Mundo.py
@@ -0,0 +1,102 @@
+#Hola mundo
+print("Hola mundo en 50 lenguajes diferentes:")
+inglés="Hello world"
+francés="Bonjour le monde"
+alemán="Hallo welt"
+italiano="Ciao mondo"
+portugués="Olá mundo"
+holandés="Hallo wereld"
+sueco="Hej världen"
+noruego="Hei verden"
+danés="Hej verden"
+finés="Hei maailma"
+ruso="Привет мир (Privet mir)"
+chino="你好 世界 (Nǐ hǎo shìjiè)"
+japonés="こんにちは 世界 (Konnichiwa sekai)"
+coreano="안녕하세요 세계 (Annyeong haseyo segye)"
+árabe=" مرحبًا بالعالم (Marhaban bialalam)"
+hebreo=" שלום עולם (Shalom olam)"
+griego="Γειά σου κόσμε (Yia sou kosme)"
+turco="Merhaba dünya"
+hindi=" नमस्ते दुनिया (Namaste duniya)"
+swahili="Hujambo dunia"
+indonesio="Halo dunia"
+malayalam="ഹലോ ലോകം (Halo lokam)"
+bengall="হ্যালো বিশ্ব (Halo bishwô)"
+vietnamita="Xin chào thế giới"
+tailandés="สวัสดี โลก (Sawasdee lok)"
+polaco="Witaj świecie"
+checo="Ahoj světe"
+eslovaco="Ahoj svet"
+húngaro="Helló világ"
+rumano="Salut lume"
+ucraniano="Привіт світ (Privit svit)"
+búlgaro="Здравей свят (Zdravey svyat)"
+croata="Pozdrav svijete"
+serbio="Здраво свете (Zdravo svete)"
+boshio="Zdravo svijete"
+letón="Sveiki pasaule"
+lituano="Labas pasauli"
+estonio="tere maailm"
+georgiano="გამარჯობა მსოფლიო (Gamarjoba msoplio)"
+kazajo="Сәлем әлем (Sälem älêm)"
+uzbeco="Salom dunyo"
+tártaro="Сәлам дөнья (Sälam dön'ya)"
+kirguís="Салам дүйнө (Salam düynö)"
+azerí="Salam, dünya"
+persa="سلام جهان (Salam jahan)"
+urdu="ہیلو دنیا (Hello dunya)"
+maratí=" नमस्ते जग (Namaste jag)"
+tamil="ஹலோ உலகம் (Halo ulagam)"
+sánscrito="नमस्ते लोक (Namaste loka)"
+tagalo="kamutsa mundo"
+print("-En inglés es:",inglés)
+print("-En francés es:",francés)
+print("-En alemán es:",alemán)
+print("-En italiano es:",italiano)
+print("-En portugués es:",portugués)
+print("-En holandés es:",portugués)
+print("-En sueco es:",sueco)
+print("-En noruego es:",noruego)
+print("-En danés es:",danés)
+print("-En finés es:",finés)
+print("-En ruso es:",ruso)
+print("-En chino es:",chino)
+print("-En japonés es:",japonés)
+print("-En coreano es:",coreano)
+print("-En árabe es:",árabe)
+print("-En hebreo es:",hebreo)
+print("-En griego es:",griego)
+print("-En turco es:",turco)
+print("-En hindi es:",hindi)
+print("-En swahili es:",swahili)
+print("-En indonesio es:",indonesio)
+print("-En malayalam es:",malayalam)
+print("-En bengall es:",bengall)
+print("-En vietnamita es:",vietnamita)
+print("-En tailandés es:",tailandés)
+print("-En polaco es:",polaco)
+print("-En checo es:",checo)
+print("-En eslovaco es:",eslovaco)
+print("-En húngaro es:",húngaro)
+print("-En rumano es:",rumano)
+print("-En ucraniano es:",ucraniano)
+print("-En búlgaro es:",búlgaro)
+print("-En croata es:",croata)
+print("-En serbio es:",serbio)
+print("-En boshio es:",boshio)
+print("-En letón es:",letón)
+print("-En lituano es:",lituano)
+print("-En estonio es:",estonio)
+print("-En georgiano es:",georgiano)
+print("-En kazajo es:",kazajo)
+print("-En uzbeco es:",uzbeco)
+print("-En tártaro es:",tártaro)
+print("-En kirguís es:",kirguís)
+print("-En azerí es:",azerí)
+print("-En persa es:",persa)
+print("-En urdu es:",urdu)
+print("-En maratí es:",maratí)
+print("-En tamil es:",tamil)
+print("-En sánscrito es:",sánscrito)
+print("-En tagalo es:",tagalo)
